a4ec930d44498241ab86ab4f9594f5c0218bd56c
murdoch <murdoch@9e42febc-bc72-dd11-a014-00d0b713cb1a>
2010-09-28 14:13:10 +0000

    Add .PostSweaveHook
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sweavesearch@1570 9e42febc-bc72-dd11-a014-00d0b713cb1a

--- pkg/patchDVI/R/SweaveAll.R
+++ pkg/patchDVI/R/SweaveAll.R
@@ -1,10 +1,19 @@
-SweaveAll <- function(SweaveFiles, make=1, ...) {
+SweaveAll <- function(SweaveFiles, make=1, PostSweaveHook=NULL, ...) {
     i <- 0
     result <- character()
     while (i < length(SweaveFiles)) {
         i <- i+1
-        suppressWarnings(remove(".SweaveFiles", ".TexRoot", envir=globalenv()))
-    	result <- c(result, Sweave(SweaveFiles[i], ...))
+        suppressWarnings(remove(".SweaveFiles", ".TexRoot", ".PostSweaveHook", 
+                                envir=globalenv()))
+        thisfile <- Sweave(SweaveFiles[i], ...)
+    	result <- c(result, thisfile)
+    	.PostSweaveHook <- PostSweaveHook
+    	if (exists(".PostSweaveHook", envir=globalenv())) 	
+    	    .PostSweaveHook <- get(".PostSweaveHook", envir=globalenv())
+    	if (!is.null(.PostSweaveHook)) {
+    	    .PostSweaveHook <- match.fun(.PostSweaveHook)
+    	    .PostSweaveHook(thisfile)
+    	}    
     	if (make && exists(".SweaveFiles", envir=globalenv())) {
     	    newfiles <- setdiff(get(".SweaveFiles", globalenv()), SweaveFiles)
             if (length(newfiles)) {
