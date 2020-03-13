7e9df1ad1ef123a809ff2f4007b2a5b32e37eb17
murdoch <murdoch@9e42febc-bc72-dd11-a014-00d0b713cb1a>
2020-03-13 22:51:00 +0000

    Update to SweaveAll to let it be more RStudio "knit" friendly.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sweavesearch@1621 9e42febc-bc72-dd11-a014-00d0b713cb1a

--- pkg/patchDVI/R/SweaveAll.R
+++ pkg/patchDVI/R/SweaveAll.R
@@ -1,6 +1,25 @@
-SweaveAll <- function(SweaveFiles, make=1, PostSweaveHook=NULL, weave=utils::Sweave, ...) {
+SweaveAll <- function(SweaveFiles = NULL, make=1, PostSweaveHook=NULL, 
+                      force = TRUE, verbose = FALSE, 
+                      weave=utils::Sweave, ...) {
+    updateSweaveFiles <- function(new, old) {
+        if (make) {
+            newfiles <- setdiff(new, old)
+            if (length(newfiles)) {
+                if (make == 1) {
+                    tex <- paste(tools::file_path_sans_ext(newfiles), ".tex", sep="")
+                    newfiles <- newfiles[!file_test("-f", tex) | file_test("-nt", newfiles, tex)]
+                } 
+                if (verbose && length(newfiles))
+                    cat("Added to SweaveFiles: ", paste(newfiles, collapse = ","), "\n")
+                SweaveFiles <<- c(old, newfiles)
+            }
+        }        
+    }
     i <- 0
     result <- character()
+    if (!force) {
+        updateSweaveFiles(SweaveFiles, NULL)
+    }
     while (i < length(SweaveFiles)) {
         i <- i+1
         suppressWarnings(remove(".SweaveFiles", ".TexRoot", ".PostSweaveHook", ".SweaveMake",
@@ -8,26 +27,18 @@ SweaveAll <- function(SweaveFiles, make=1, PostSweaveHook=NULL, weave=utils::Swe
         thisfile <- weave(SweaveFiles[i], ...)
     	result <- c(result, thisfile)
     	.PostSweaveHook <- PostSweaveHook
-    	if (exists(".PostSweaveHook", envir=globalenv())) 	
-    	    .PostSweaveHook <- get(".PostSweaveHook", envir=globalenv())
-    	if (exists(".SweaveMake", envir=globalenv()))
-    	    make <- get(".SweaveMake", envir=globalenv())
+    	if (exists(".PostSweaveHook", envir=globalenv(), inherits = FALSE)) 	
+    	    .PostSweaveHook <- get(".PostSweaveHook", envir=globalenv(), inherits = FALSE)
+    	if (exists(".SweaveMake", envir=globalenv(), inherits = FALSE))
+    	    make <- get(".SweaveMake", envir=globalenv(), inherits = FALSE)
     	if (!is.null(.PostSweaveHook)) {
     	    .PostSweaveHook <- match.fun(.PostSweaveHook)
     	    .PostSweaveHook(thisfile)
-    	}    
-    	if (make && exists(".SweaveFiles", envir=globalenv())) {
-    	    newfiles <- setdiff(get(".SweaveFiles", globalenv()), SweaveFiles)
-            if (length(newfiles)) {
-            	if (make == 1) {
-            	    tex <- paste(tools::file_path_sans_ext(newfiles), ".tex", sep="")
-            	    SweaveFiles <- c(SweaveFiles, newfiles[!file_test("-f", tex) | file_test("-nt", newfiles, tex)])
-            	} else 
-            	    SweaveFiles <- c(SweaveFiles, newfiles)
-            }
-        }
-        if (exists(".TexRoot", envir=globalenv())) {
-            TexRoot <- get(".TexRoot", globalenv())
+    	}
+    	if (exists(".SweaveFiles", envir = globalenv(), inherits = FALSE))
+    	  updateSweaveFiles(get(".SweaveFiles", globalenv(), inherits = FALSE), SweaveFiles)
+        if (exists(".TexRoot", envir=globalenv(), inherits = FALSE)) {
+            TexRoot <- get(".TexRoot", globalenv(), inherits = FALSE)
             result <- c(TexRoot, result[result != TexRoot])
         }
     }
